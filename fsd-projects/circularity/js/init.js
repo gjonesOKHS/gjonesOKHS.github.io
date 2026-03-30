89cb7e696ee77348e82c5011aa8a0b01dceba227
gjonesOKHS <gabejones35@yahoo.com>
2026-03-30 14:18:11 +0000

    html

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,11 +20,16 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+     var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+    function drawCircle(){
+     circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+     physikz.addRandomVelocity(circle, canvas, 5, 5);
+     view.addChild(circle);
+     circles.push(circle);   
+    }
 
 
         // TODO 3 : Call the drawCircle() function
