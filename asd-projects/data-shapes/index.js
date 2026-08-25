b11d7225543904a7496e26beff0c79e15f45304b
gjonesOKHS <gabejones35@yahoo.com>
2026-08-25 13:09:38 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-  handleStatic(currentShape);
+  handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
